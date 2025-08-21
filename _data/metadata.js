09f68607641eeaea5a39403146b4bb8839737a21
Yatan Blu <yatanblu@gmail.com>
2025-08-21 16:12:41 -0500

    fixed sitemap

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 export default {
 	title: "Living Rhea",
-	url: "https://example.com/",
+	url: "https://livingrhea.com/",
 	language: "en",
 	description: "Rhea has the drive of an entrepreneur, the mind of a scientist and the heart of a healer. She has a focused vision and an impressive speed of execution. This is due in part to her unwavering commitment, her natural ability as a community builder and leader, and the rigour she brings to the products, ideas, and services she creates. She makes an excellent collaborator and we are just witnessing the start of her success. Big things to come.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Dr Rhea Mehta",
+		url: "https://livingrhea.com/about/"
 	}
 }
